778a74065dc58a3e8d654057793381fd53862ac2
EMINENCE <Emmyfedev@gmail.com>
2022-05-12 12:22:05 -0700

    fixing cors issue

--- src/App.js
+++ src/App.js
@@ -13,11 +13,11 @@ const   [selectedPunk, setSelectedPunk] = useState(0);
 
 useEffect(()=>{
   const getnft = async () =>{
-  const  activepunks = await axios.get(
+ await axios.get(
     'https://testnets-api.opensea.io/api/v1/assets?asset_contract_address=0x814cCA3cAd4Fe0335aCC9211021aE00A4F14226e&order_direction=asc&offset=0'
+    ).then(activepunk  =>  setPunkListData(activepunk.data.assets)
     )
 
-      setPunkListData(activepunks.data.assets);
      
       }
 
